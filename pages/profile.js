20925be3d1fd8cdad29c3c48bc58c8397585ab22
Ryuyxx <Ryuyxx@gmail.com>
2021-04-20 00:03:47 +0900

    :broom:

--- pages/profile.js
+++ pages/profile.js
@@ -4,7 +4,8 @@ import Image from "next/image";
 export default function Profile() {
     return (
         <Layout title="Profile">
-            <div className="bg-white text-center shadow-xl p-8 w-80 rounded-2xl dark:bg-gray-800 h-96">
+            <div
+                className="bg-white text-center shadow-xl p-8 w-72 rounded-2xl dark:bg-gray-800 h-96 hover:shadow-2xl duration-500">
                 <div className="mt-4">
                     <p className="font-bold">Profile Card</p>
                 </div>
@@ -18,12 +19,12 @@ export default function Profile() {
                     />
                 </div>
                 <div className="mt-4">
-                    <p className="font-bold my-1 text-gray-600">Hoi Ching Chan</p>
-                    <p className="text-xs my-1 text-gray-600">Fashion Model</p>
+                    <p className="font-bold my-1 mt-6 text-gray-600">Hoi Ching Chan</p>
+                    <p className="text-xs mb-4 text-gray-600 ">Designer Model</p>
                     <p className="font-bold">Location</p>
-                    <p className="text-xs my-1 text-gray-600">Tokyo, Japan</p>
+                    <p className="text-xs mb-4 text-gray-600">Tokyo, Japan</p>
                     <p className="font-bold">E-mail</p>
-                    <p className="text-xs my-1 text-gray-600">ruizu@gmail.com</p>
+                    <p className="text-xs mb-4 text-gray-600">ruizu@gmail.com</p>
 
                 </div>
                 <div className="mt-8 flex justify-around">
@@ -42,11 +43,11 @@ export default function Profile() {
                     </div>
                     <div>
                         <a
-                            href=""
-                            target="_blank"
+                            href="#"
+                            // target="_blank"
                         >
                             <svg
-                                className="w-6 h-6 fill-current text-black-500 inset-0 transform  hover:scale-75 transition duration-300"
+                                className="w-6 h-6 fill-current text-black-500 inset-0 transform  hover:scale-75 transition duration-300 opacity-30 cursor-not-allowed"
                                 fill="currentColor" viewBox="0 0 20 20">
                                 <path
                                     d="M18.258,3.266c-0.693,0.405-1.46,0.698-2.277,0.857c-0.653-0.686-1.586-1.115-2.618-1.115c-1.98,0-3.586,1.581-3.586,3.53c0,0.276,0.031,0.545,0.092,0.805C6.888,7.195,4.245,5.79,2.476,3.654C2.167,4.176,1.99,4.781,1.99,5.429c0,1.224,0.633,2.305,1.596,2.938C2.999,8.349,2.445,8.19,1.961,7.925C1.96,7.94,1.96,7.954,1.96,7.97c0,1.71,1.237,3.138,2.877,3.462c-0.301,0.08-0.617,0.123-0.945,0.123c-0.23,0-0.456-0.021-0.674-0.062c0.456,1.402,1.781,2.422,3.35,2.451c-1.228,0.947-2.773,1.512-4.454,1.512c-0.291,0-0.575-0.016-0.855-0.049c1.588,1,3.473,1.586,5.498,1.586c6.598,0,10.205-5.379,10.205-10.045c0-0.153-0.003-0.305-0.01-0.456c0.7-0.499,1.308-1.12,1.789-1.827c-0.644,0.28-1.334,0.469-2.06,0.555C17.422,4.782,17.99,4.091,18.258,3.266"/>
@@ -55,12 +56,11 @@ export default function Profile() {
                     </div>
                     <div>
                         <a
-                            // href="https://www.behance.net/ruizz"
-                            href="#"
-                            // target="_blank"
+                            href="https://www.behance.net/ruizz"
+                            target="_blank"
                         >
                             <svg
-                                className="w-6 h-6 fill-current text-black-100 inset-0 transform  hover:scale-75 transition duration-300 opacity-30 cursor-not-allowed"
+                                className="w-6 h-6 fill-current text-black-100 inset-0 transform  hover:scale-75 transition duration-300"
                                 xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="95.802px" height="95.802px"
                                 viewBox="0 0 95.802 95.802">
                                 <g>
